daed1b7da410fd16bdbb7623173b6ce617b6612d
Valeria <lerik13@gmail.com>
2025-03-07 17:00:58 -0700

    Loading & Not-found Page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,11 @@
-import { Button } from "@/components/ui/button";
+//const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
 
 const HomePage = () => {
+	//await delay(2000)
+
 	return (
 	<>
-		<h1>e-commerce</h1>
+		<h1>e-Store</h1>
 	</>
 	);
 }
