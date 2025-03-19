32bc4fa0b4f7cffa337314b147acaebe62adccd8
Valeria <lerik13@gmail.com>
2025-03-19 16:44:32 -0600

    Checkout Steps Component

--- app/(root)/shipping-address/page.tsx
+++ app/(root)/shipping-address/page.tsx
@@ -5,6 +5,7 @@ import { ShippingAddress } from '@/types'
 import { getMyCart } from '@/lib/actions/cart.actions'
 import { getUserById } from '@/lib/actions/user.actions'
 import ShippingAddressForm from './shipping-address-form'
+import CheckoutSteps from '@/components/cart/checkout-steps'
 
 export const metadata: Metadata = {
   title: 'Shipping Address',
@@ -24,6 +25,7 @@ const ShippingAddressPage = async () => {
 
   return (
     <>
+      <CheckoutSteps current={1} />
       <ShippingAddressForm address={user.address as ShippingAddress} />
     </>
   )
