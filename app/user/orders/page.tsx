f7d657f4d4e49fe0a347835265a365fa923d2467
Valeria <lerik13@gmail.com>
2025-03-27 15:04:58 -0600

    Orders Pagination

--- app/user/orders/page.tsx
+++ app/user/orders/page.tsx
@@ -10,6 +10,7 @@ import {
 } from '@/components/ui/table'
 import { formatCurrency, formatDateTime, formatId } from '@/lib/utils'
 import Link from 'next/link'
+import Pagination from '@/components/pagination'
 
 export const metadata: Metadata = {
   title: 'My orders',
@@ -66,6 +67,12 @@ const OrdersPage = async (props: {
             ))}
           </TableBody>
         </Table>
+        {orders.totalPages > 1 && (
+          <Pagination
+            page={Number(page) || 1}
+            totalPages={orders?.totalPages}
+          />
+        )}
       </div>
     </div>
   )
