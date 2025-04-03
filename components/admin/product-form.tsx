576cd619bbca80945e9b1452b358403055ed9c35
Valeria <lerik13@gmail.com>
2025-04-03 14:02:42 -0600

    Admin: Create Product page: Submission

--- components/admin/product-form.tsx
+++ components/admin/product-form.tsx
@@ -5,7 +5,7 @@ import { insertProductSchema, updateProductSchema } from '@/lib/validators'
 import { Product } from '@/types'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { useRouter } from 'next/navigation'
-import { ControllerRenderProps, useForm } from 'react-hook-form'
+import { ControllerRenderProps, SubmitHandler, useForm } from 'react-hook-form'
 import slugify from 'slugify'
 import { z } from 'zod'
 import { Button } from '../ui/button'
@@ -19,6 +19,8 @@ import {
 } from '../ui/form'
 import { Input } from '../ui/input'
 import { Textarea } from '../ui/textarea'
+import { createProduct, updateProduct } from '@/lib/actions/product.actions'
+import { toast } from 'sonner'
 
 const ProductForm = ({
   type,
@@ -39,9 +41,38 @@ const ProductForm = ({
       product && type === 'Update' ? product : productDefaultValues,
   })
 
+  const onSubmit: SubmitHandler<z.infer<typeof insertProductSchema>> = async (
+    values
+  ) => {
+    if (type === 'Create') {
+      const res = await createProduct(values)
+      if (!res.success) toast.error(res.message)
+
+      toast.message(res.message)
+      router.push('/admin/products')
+    }
+    // On Update
+    if (type === 'Update') {
+      if (!productId) {
+        router.push('/admin/products')
+        return
+      }
+
+      const res = await updateProduct({ ...values, id: productId })
+      if (!res.success) toast.error(res.message)
+
+      toast.message(res.message)
+      router.push('/admin/products')
+    }
+  }
+
   return (
     <Form {...form}>
-      <form className='space-y-8'>
+      <form
+        method='POST'
+        onSubmit={form.handleSubmit(onSubmit)}
+        className='space-y-8'
+      >
         <div className='flex flex-col md:flex-row gap-5'>
           {/* Name */}
           <FormField
