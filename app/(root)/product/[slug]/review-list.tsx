e1ffb512e9ddc5e0c9bb9d3364c1603c5374fc1c
Valeria <lerik13@gmail.com>
2025-04-23 17:04:37 -0600

    Review List component

---
+++ app/(root)/product/[slug]/review-list.tsx
@@ -0,0 +1,40 @@
+'use client'
+
+import { Review } from '@/types'
+import Link from 'next/link'
+import { useState } from 'react'
+
+const ReviewList = ({
+  userId,
+  productId,
+  productSlug,
+}: {
+  userId: string
+  productId: string
+  productSlug: string
+}) => {
+  const [reviews, setReviews] = useState<Review[]>([])
+
+  return (
+    <div className='space-y-4'>
+      {reviews.length === 0 && <div>No reviews yet</div>}
+      {userId ? (
+        <>{/* REVIEW FORM */}</>
+      ) : (
+        <div>
+          Please
+          <Link
+            href={`/sign-in?callbackUrl=/product/${productSlug}`}
+            className='text-blue-700 px-2'
+          >
+            sign in
+          </Link>
+          to write a review
+        </div>
+      )}
+      <div className='flex flex-col gap-3'>{/* REVIEWS HERE */}</div>
+    </div>
+  )
+}
+
+export default ReviewList
