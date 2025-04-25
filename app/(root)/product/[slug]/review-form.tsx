9a2cbabcffd73ca0b0ef49bb9401066079340afa
Valeria <lerik13@gmail.com>
2025-04-24 18:04:19 -0600

    Review: connect action with form

--- app/(root)/product/[slug]/review-form.tsx
+++ app/(root)/product/[slug]/review-form.tsx
@@ -26,12 +26,14 @@ import {
   SelectValue,
 } from '@/components/ui/select'
 import { Textarea } from '@/components/ui/textarea'
+import { createUpdateReview } from '@/lib/actions/review.actions'
 import { ReviewFormDefaultValues } from '@/lib/constants'
 import { insertReviewSchema } from '@/lib/validators'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { StarIcon } from 'lucide-react'
 import { useState } from 'react'
-import { useForm } from 'react-hook-form'
+import { SubmitHandler, useForm } from 'react-hook-form'
+import { toast } from 'sonner'
 import { z } from 'zod'
 
 const ReviewForm = ({
@@ -41,7 +43,7 @@ const ReviewForm = ({
 }: {
   userId: string
   productId: string
-  onReviewSubmitted?: () => void
+  onReviewSubmitted: () => void
 }) => {
   const [open, setOpen] = useState(false)
 
@@ -50,8 +52,29 @@ const ReviewForm = ({
     defaultValues: ReviewFormDefaultValues,
   })
 
+  // Open Form Handler
   const handleOpenForm = () => {
+    form.setValue('productId', productId)
+    form.setValue('userId', userId)
+
     setOpen(true)
+  }
+
+  // Submit Form Handler
+  const onSubmit: SubmitHandler<z.infer<typeof insertReviewSchema>> = async (
+    values
+  ) => {
+    const res = await createUpdateReview({ ...values, productId })
+
+    if (!res.success) {
+      return toast.error(res.message)
+    }
+
+    setOpen(false)
+
+    onReviewSubmitted()
+
+    toast.message(res.message)
   }
 
   return (
@@ -61,7 +84,7 @@ const ReviewForm = ({
       </Button>
       <DialogContent className='sm:max-w-[425px]'>
         <Form {...form}>
-          <form method='post'>
+          <form method='post' onSubmit={form.handleSubmit(onSubmit)}>
             <DialogHeader>
               <DialogTitle>Write a Review</DialogTitle>
               <DialogDescription>
